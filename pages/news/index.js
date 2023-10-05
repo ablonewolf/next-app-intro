8a829146eb786039c45a2af91aa574e6d27f1bd0
Arka Bhuiyan <arka.bhuiyan@technonext.com>
2023-10-05 15:49:44 +0600

    linked news page and detail page.

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,21 @@
+import {Fragment} from "react";
+import Link from "next/link";
+
 const NewsPage = () => {
     return (
-        <h1>News Page Header</h1>
+        <Fragment>
+            <h1>News Page Header</h1>
+            <ul>
+                <li>
+                    <Link href={'/news/detail'}>
+                        Detail page
+                    </Link>
+                </li>
+                <li>
+                    Some Other page
+                </li>
+            </ul>
+        </Fragment>
     )
 };
 export default NewsPage;
