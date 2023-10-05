9919f203322143e7e57739b6134676ff91dda7a6
Arka Bhuiyan <arka.bhuiyan@technonext.com>
2023-10-05 15:24:54 +0600

    logged the dynamic route data to the console.

--- pages/news/[details].js
+++ pages/news/[details].js
@@ -1,4 +1,8 @@
+import {useRouter} from "next/router";
+
 const DetailsPage = () => {
+    const router = useRouter();
+    console.log(router.query.details);
     return (
         <h1>The Details Page Header</h1>
     )
