911621643bb3e91282213bcc18df5ca4474b3e40
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-10-07 23:41:47 +0600

    Created a page to show dummy meetup items.

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,12 +1,17 @@
-import Card from '@/components/ui/Card';
+import {Card} from '@/components/ui/Card';
 import classes from './MeetupItem.module.css';
 
 export const MeetupItem = (props) => {
     return (
         <li className={classes.item}>
             <Card>
-                <div className={classes.image}>
-                    <img src={props.image} alt={props.title}/>
+                <div className={classes.imageContainer}>
+                    <img
+                        className={classes.image}
+                        style={{maxWidth: '800px', height: 'auto'}}
+                        src={props.image}
+                        alt={props.title}
+                    />
                 </div>
                 <div className={classes.content}>
                     <h3>{props.title}</h3>
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import {MeetupList} from "@/components/meetups/MeetupList";
+
+const demoMeetupList = [
+    {
+        id: 1,
+        title: "Regular Hangout Place",
+        image: 'https://upload.wikimedia.org/wikipedia/commons/b/b5/Curzon_Hall_Panorama.jpg',
+        address: "Curzon Hall, Doyel Circle, University of Dhaka, Dhaka",
+        description: "Central Exam hall of Curzon Hall"
+    },
+    {
+        id: 2,
+        title: "Second Preferred hangout zone",
+        image: 'https://prod-media-eng.dhakatribune.com/uploads/2020/12/tsc-1608390694064.jpg',
+        address: "TSC, University of Dhaka, Dhaka",
+        description: "The inner field of TSC"
+    }
+]
+
+function HomePage() {
+    return <MeetupList meetups={demoMeetupList}/>
+}
+
+export default HomePage;
