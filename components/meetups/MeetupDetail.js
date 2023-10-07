73900bf2a376bbc2bceb6b0db4358b6daca53455
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-10-08 04:57:54 +0600

    added a button to go back to the home page along with its styling.

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,6 +1,11 @@
 import classes from './MeetupDetail.module.css'
+import {useRouter} from "next/router";
 
 export const MeetupDetail = (props) => {
+    const router = useRouter();
+    const showHomePageHandler = () => {
+        router.push('/');
+    }
     return (
         <section className={classes.detail}>
             <img
@@ -10,6 +15,7 @@ export const MeetupDetail = (props) => {
             <h1>{props.meetupDetail.title}</h1>
             <address>{props.meetupDetail.address}</address>
             <p>{props.meetupDetail.description}</p>
+            <button onClick={showHomePageHandler}>Go Back</button>
         </section>
     )
 }
